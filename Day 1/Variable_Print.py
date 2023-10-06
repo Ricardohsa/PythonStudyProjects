f78c256678359ce99d57958a9436e410d7e75ca4
Ricardo Humberto Sá <r.humberto.sa@gmail.com>
2023-10-06 13:40:47 -0400

    Initial project

---
+++ Day 1/Variable_Print.py
@@ -0,0 +1,27 @@
+# creating and print a new line '\n'
+print("Ricardo Humberto Sa\n3 Novello Court\nPostal code N6J 3B1")
+
+# Concact Strings
+
+# input Function
+# print('Hello ' + input("What's your name: "))
+
+# Counting caracters
+name = input("What is your name?")
+lenght = len(name)
+
+# variables
+print(lenght)
+
+#1. Create a greeting for your program.
+print("Hello, Hello Welcome to the Badn name generator 2000 Master")
+#2. Ask the user for the city that they grew up in.
+city = input("What city you grow up?\n")
+#3. Ask the user for the name of a pet.
+pet = input("What is your pet name?\n")
+
+#4. Combine the name of their city and pet and show them their band name.
+print("Your band name could be: " + city + ' ' + pet)
+#5. Make sure the input cursor shows on a new line:
+
+# Solution: https://replit.com/@appbrewery/band-name-generator-end

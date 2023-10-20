50d31ec6c9234e07adc0299b45bdc938e024a2ee
Ricardo Humberto Sá <r.humberto.sa@gmail.com>
2023-10-20 09:02:13 -0400

    Birthday Program - Reads a csv file and send out an email wishing happy birthday from a random letter.

---
+++ Day 32- Email Sender/quote_of_day.py
@@ -0,0 +1,24 @@
+import random as rd
+import datetime as dt
+import smtplib as smtp
+
+current_day = dt.datetime.now()
+my_email = "python11041982@gmail.com"
+password = "hyma kizy koiy pggn"
+to = "r.humberto.sa@gmail.com"
+
+
+
+
+with open("quotes.txt", "r") as file:
+    data = file.read()
+
+    if current_day.weekday() == 3:
+        data_into_list = data.replace('"', '').split('\n')
+
+        with smtp.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            connection.sendmail(from_addr=my_email,
+                                to_addrs=to,
+                                msg=f"Subject:Hello\n\n {rd.choice(data_into_list)}")

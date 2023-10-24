34a630b9a320deee864db79e46ed69ed66c2dd92
Ricardo Humberto Sá <r.humberto.sa@gmail.com>
2023-10-24 10:28:47 -0400

    dict ascending and descending

--- Dictionary/main.py
+++ Dictionary/main.py
@@ -1,1 +1,11 @@
-my_dic = {}
+
+
+# Write a Python script to sort (ascending and descending) a dictionary by value.
+# dictionary = {1: 2, 3: 4, 4: 3, 2: 1, 0: 0}
+
+# ascending = dict(sorted(dictionary.items(), key=lambda item: item[1]))
+
+# descending = dict(sorted(dictionary.items(), key=lambda item: item[1], reverse=True))
+# print(ascending)
+
+# print(descending)
